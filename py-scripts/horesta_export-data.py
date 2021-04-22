27b0b5707c4e956b306d2ef7ac4801b6a3c175d7
Kristian Gade Kjelmann <kgk@adm.aau.dk>
2021-04-22 15:34:43 +0200

    adding date and tags to output texts

--- py-scripts/horesta_export-data.py
+++ py-scripts/horesta_export-data.py
@@ -20,16 +20,20 @@ df = pd.DataFrame.from_records(data)
     
 df_select = df.loc[:, df.columns != 'html']
 df_select['text'] = df_select['text'].apply(lambda string: string.replace("\n", ""))
-df_select.to_csv(os.path.join(data_path, outname), index = False)
+#df_select.to_csv(os.path.join(data_path, outname), index = False)
 
 
 # Exporting texts
 
 df_export = df.copy()
 df_export['publish_date'] = df_export['publish_date'].str.replace(' ', '')
-df_export['text'] = df_export['title'] + df_export['text']
+#df_export['text'] = df_export['title'] + df_export['text']
 
-outdir = os.path.join(data_path, 'txt')
+outdir = os.path.join(data_path, 'txt_with-meta')
+
+if not os.path.isdir(outdir):
+    os.mkdir(outdir)
+
 old_postdate = ""
 counter = 1
 
@@ -41,17 +45,22 @@ for postindex in list(df_export.index):
     day = postdate[0:2]
     
     postdate = year + "-" + month + "-" + day
-    
     indexstr = str(postindex).rjust(4, '0')
-        
     filename = "horesta_{date}_{index}.txt".format(date = postdate, index = indexstr)
     
     old_postdate = postdate
-
+    
+    # Create text with title, date, tags
+    title = str(df_export.loc[postindex, 'title'])
+    text = str(df_export.loc[postindex, 'text'])
+    tags = str(df_export.loc[postindex, 'tags'])
+    
+    text_out = f"{title} \n\npublish_date:\t{str(postdate)}\n\ntags:\t{tags}\n\n{text}"
+    
     outpath = os.path.join(outdir, filename)
     
     if os.path.isfile(outpath):
         continue
     
     with open(outpath, 'w', encoding = "utf-8") as f:
-        f.write(str(df_export.loc[postindex, 'text']))
+        f.write(text_out)
